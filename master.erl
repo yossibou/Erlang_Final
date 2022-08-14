1a2d4c4bd20bee2d830e630e59db1d8f4d40d55c
yossibou <62253733+yossibou@users.noreply.github.com>
2022-08-14 22:25:32 +0300

    Update master.erl

--- master.erl
+++ master.erl
@@ -17,7 +17,7 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 start() ->
-    Return = gen_server:start_link({local, master}, ?MODULE, [], [timeout]),
+    Return = gen_server:start_link({local, master}, ?MODULE, [], [{timeout,500}]),
     Return.
 stop() ->
 
