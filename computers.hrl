9111f2f93aa3b6b32826a11ef18bad8c32fbe0cb
yossibou <62253733+yossibou@users.noreply.github.com>
2022-08-15 18:09:52 +0300

    Update computers.hrl

--- computers.hrl
+++ computers.hrl
@@ -1,8 +1,8 @@
 
--define(PC1, 'PC1@132.72.81.60').
--define(PC2, 'PC2@132.72.81.167').
--define(PC3, 'PC3@132.72.81.85').
--define(PC4, 'PC4@132.72.81.224').
+-define(PC1, 'PC5@132.72.81.60').
+-define(PC2, 'PC6@132.72.81.167').
+-define(PC3, 'PC7@132.72.81.85').
+-define(PC4, 'PC8@132.72.81.224').
 -define(MASTER,'home@132.72.81.60').
 
 %erl -setcookie dough -name home@132.72.81.60
